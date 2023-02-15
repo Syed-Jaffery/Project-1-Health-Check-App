979f865e6af35c5019f03240783db11d1ee97ccf
Syed Jaffery <sasjaffery@gmail.com>
2023-02-15 17:10:05 +0000

    add input fields

---
+++ index.js
@@ -0,0 +1,15 @@
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '90f5491a77msh4f438d805de16cdp1a5061jsn0fe1b96543a8',
+    'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com',
+  },
+};
+
+fetch(
+  'https://fitness-calculator.p.rapidapi.com/bmi?age=25&weight=65&height=180',
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
