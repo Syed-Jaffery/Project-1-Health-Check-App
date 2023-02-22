14921d6679f64e301a7d5711ce39a438f99d7e01
Syed Jaffery <sasjaffery@gmail.com>
2023-02-22 20:37:35 +0000

    test APIs

--- index.js
+++ index.js
@@ -28,38 +28,46 @@ const options = {
   },
 };
 
-// let ageValue = document.getElementById('age').value;
-// let weightValue = document.getElementById('weight').value;
-// let heightValue = document.getElementById('height').value;
+const ageValue = document.getElementById('ageValue');
+const weightValue = document.getElementById('weightValue');
+const heightValue = document.getElementById('heightValue');
+const bmiBtn = document.getElementById('bmi-btn');
 
-let ageValue = 30;
-let weightValue = 100;
-let heightValue = 180;
-let queryURL = `https://fitness-calculator.p.rapidapi.com/bmi?age=${ageValue}&weight=${weightValue}&height=${heightValue}`;
+let bmiResult = document.getElementById('bmi-result');
+let bmiCatergory = document.getElementById('bmi-catergory');
+let previousResult = document.getElementById('previous-result');
 
-fetch(queryURL, options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+bmiBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let age = parseInt(ageValue.value);
+  let weight = parseInt(weightValue.value);
+  let height = parseInt(heightValue.value);
 
-// clear button
-function clear() {
-  window.location.clear();
-}
+  const queryURL = `https://fitness-calculator.p.rapidapi.com/bmi?age=${age}&weight=${weight}&height=${height}`;
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '90f5491a77msh4f438d805de16cdp1a5061jsn0fe1b96543a8',
-    'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com',
-  },
-};
+  fetch(queryURL, options)
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error(err));
+});
 
-let gender = 'male';
-let height = 180;
-let queryUrl = `https://fitness-calculator.p.rapidapi.com/idealweight?gender=${gender}&height=${height}`;
+// // clear button
+// function clear() {
+//   window.location.clear();
+// }
 
-fetch(queryUrl, options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+const genderValue = document.getElementById('genderValue');
+const ibwHeightValue = document.getElementById('ibwHeightValue');
+
+weightBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let gender = genderValue.value;
+  let ibwHeight = parseInt(ibwHeightValue.value);
+
+  const queryURL = `https://fitness-calculator.p.rapidapi.com/idealweight?gender=${gender}&height=${ibwHeight}`;
+
+  fetch(queryURL, options)
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error(err));
+});
